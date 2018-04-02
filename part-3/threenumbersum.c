28eb407ccbbb660db9781efd5c55392a7f1dca8d
MD: ALOMGIR HOSSEN <alombd.bd@gmail.com>
2018-04-02 08:41:50 -0700

    Create threenumbersum.c

---
+++ part-3/threenumbersum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+#include <conio.h>
+ 
+main () 
+   
+ {
+ 
+
+   int a, b, c sum;
+ 
+   printf("Enter three number :");
+ 
+   scanf("%d %d %d", &a, &b, &c);
+ 
+   sum = a + b + c;
+ 
+   printf("The result of sum is : %d", sum);
+ 
+   return 0;
+    
+ }
